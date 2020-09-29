2da017317e5aa0350fa117b9e86b9c71b0260b1a
Kris Casper <sw.darkwind@gmail.com>
2020-09-29 20:39:21 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+function getBracketNum(char, bracketsConfig, isOpen = true) {
+  for(let bracketNum = 0; bracketNum < bracketsConfig.length; bracketNum++) {
+    if(bracketsConfig[bracketNum][Number(!isOpen)] == char) return bracketNum
+  }
+  return -1
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  for(const char of str) {
+    let bracketOpen = getBracketNum(char, bracketsConfig)
+    let bracketClose = getBracketNum(char, bracketsConfig, false)
+
+    if(bracketClose === stack[stack.length-1]) stack.pop()
+    else if(bracketOpen >= 0) stack.push(bracketOpen)
+    else return false
+  }
+  return stack.length == 0
 }
